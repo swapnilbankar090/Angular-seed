53e3171caff72e8b7c733724b36a9b5a568bb1f6
swapnilbankar090 <swapnilbankar090@gmail.com>
2024-12-05 14:46:25 +0530

    book

--- AngularDay_3/Booking_project/src/app/app.component.ts
+++ AngularDay_3/Booking_project/src/app/app.component.ts
@@ -1,12 +1,20 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,RouterLink,CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'Booking_project';
+
+  showNavbar: boolean = false;
+
+  // Toggle the visibility of the navbar items
+  showAll() {
+    this.showNavbar = !this.showNavbar;
+  }
 }
--- AngularDay_3/Booking_project/src/app/app.routes.ts
+++ AngularDay_3/Booking_project/src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { HotelComponent } from './hotel/hotel.component';
+import { BusComponent } from './bus/bus.component';
+import { CarComponent } from './car/car.component';
+import { TrainComponent } from './train/train.component';
+import { FlightComponent } from './flight/flight.component';
+import { ErrorComponent } from './error/error.component';
+import { HomeComponent } from './home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: HomeComponent },
+    { path: 'hotel', component: HotelComponent },
+    { path: 'bus', component: BusComponent },
+    { path: 'car', component: CarComponent },
+    { path: 'train', component: TrainComponent },
+    { path: 'flight', component: FlightComponent },
+    { path: '**', component: ErrorComponent },
+
+];
